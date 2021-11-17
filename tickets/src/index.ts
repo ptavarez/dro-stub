127b8b22375a21531d73bd64e9db827c5437288c
Pedro Tavarez <ptavarez16@gmail.com>
2021-11-16 21:55:33 -0500

    Add a bunch of stuff lol

---
+++ tickets/src/index.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.info('Connected to Tickets Database');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.info('Tickets service is up and running!');
+  });
+};
+
+start();
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,50 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import request from 'supertest';
+
+import { app } from '../app';
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'yolo';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'pe@dro.com',
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`express:sess=${base64}`];
+};
