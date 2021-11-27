f0059de4dfcab61523e4b3c85a0aa69ab6c23e54
Pedro Tavarez <32114629+ptavarez@users.noreply.github.com>
2021-11-27 09:56:16 -0500

    Set up NATS (Event Bus) (#3)

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -6,7 +6,10 @@ import {
   NotFoundError,
   NotAuthorizedError,
 } from '@drotickets/common';
+
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -36,6 +39,13 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
