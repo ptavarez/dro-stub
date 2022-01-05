00bf6f627e9d3a9c037d85ffa060366107ac0945
Pedro Tavarez <32114629+ptavarez@users.noreply.github.com>
2022-01-05 15:11:25 -0500

    Update index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -13,7 +13,7 @@ const start = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.info('Connected to Auth Database');
+    console.info('Connected to Auth Database!');
   } catch (err) {
     console.error(err);
   }
