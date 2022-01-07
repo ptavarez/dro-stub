753c7e1a2db2d8edf4a16e2ca1583eb125a96282
Pedro Tavarez <dro@onroot.io>
2022-01-06 22:28:35 -0500

    Add

--- client/components/header.js
+++ client/components/header.js
@@ -4,27 +4,27 @@ const Header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'Sell Stubs', href: '/tickets/new' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
     currentUser && { label: 'My Orders', href: '/orders' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => (
-      <li key={href} className='nav-item'>
+      <li key={href} className="nav-item">
         <Link href={href}>
-          <a className='nav-link link-light'>{label}</a>
+          <a className="nav-link link-light">{label}</a>
         </Link>
       </li>
     ));
 
   return (
-    <nav className='navbar navbar-dark bg-dark'>
-      <div className='container'>
-        <Link href='/'>
-          <a className='navbar-brand'>DroStub</a>
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <Link href="/">
+          <a className="navbar-brand">DroStub</a>
         </Link>
-        <div className='d-flex justify-content-end'>
-          <ul className='nav d-flex align-items-center'>{links}</ul>
+        <div className="d-flex justify-content-end">
+          <ul className="nav d-flex align-items-center">{links}</ul>
         </div>
       </div>
     </nav>
