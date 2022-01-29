9c3f16d2a74651bb36157549d4a0bd3c49ffac87
Pedro Tavarez <32114629+ptavarez@users.noreply.github.com>
2022-01-29 14:24:04 -0500

    Update Deploy Actions with Correct String Syntax (#20)
    * Update Deploy Actions with Correct String Syntax
    
    * Update Base URL for Client
    
    * Update log

--- client/pages/api/build-client.js
+++ client/pages/api/build-client.js
@@ -1,18 +1,25 @@
 import axios from 'axios';
 
-export const buildClient = ({ req }) => {
-  // Server side rendering (SSR)
-  if (typeof window === 'undefined') {
-    const axiosInstance = axios.create({
-      baseURL: 'http://www.drostub.co',
-      headers: req.headers,
-    });
+const SSR_BASE_URL =
+	process.env.NODE_ENV === 'development'
+		? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+		: 'http://www.drostub.co';
 
-    return axiosInstance;
-  } else {
-    // client
-    return axios.create({
-      baseURL: '/',
-    });
-  }
+console.log(`Building client in ${process.env.NODE_ENV} mode`);
+
+export const buildClient = ({ req }) => {
+	// Server side rendering (SSR)
+	if (typeof window === 'undefined') {
+		const axiosInstance = axios.create({
+			baseURL: SSR_BASE_URL,
+			headers: req.headers,
+		});
+
+		return axiosInstance;
+	} else {
+		// client
+		return axios.create({
+			baseURL: '/',
+		});
+	}
 };
