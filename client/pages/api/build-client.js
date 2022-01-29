930c49fc9a80aa34ce7ada1f3d20b308b1d251fe
Pedro Tavarez <ptavarez16@gmail.com>
2022-01-29 14:16:46 -0500

    Update Base URL for Client

--- client/pages/api/build-client.js
+++ client/pages/api/build-client.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const SSR_BASE_URL = process.env.NODE_ENV
-	? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
-	: 'http://www.drostub.co';
+const SSR_BASE_URL =
+	process.env.NODE_ENV === 'development'
+		? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+		: 'http://www.drostub.co';
 
 export const buildClient = ({ req }) => {
 	// Server side rendering (SSR)
 	if (typeof window === 'undefined') {
+		console.log(`Server Side Rendering Client in ${process.env.NODE_ENV} mode`);
 		const axiosInstance = axios.create({
 			baseURL: SSR_BASE_URL,
 			headers: req.headers,
