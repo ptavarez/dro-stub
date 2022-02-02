0cd7f45861f6f2dc2df6672b6e0b6b74773a41ea
Pedro Tavarez <ptavarez16@gmail.com>
2022-02-01 21:39:36 -0500

    Showcase

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,39 +1,39 @@
 import Link from 'next/link';
 
 const LandingPage = ({ currentUser, tickets }) => {
-  const ticketList = tickets.map((ticket) => {
-    return (
-      <tr key={ticket.id}>
-        <td>
-          <Link href='/tickets/[ticketId]' as={`/tickets/${ticket.id}`}>
-            <a>{ticket.title}</a>
-          </Link>
-        </td>
-        <td>{ticket.price}</td>
-      </tr>
-    );
-  });
+	const ticketList = tickets.map((ticket) => {
+		return (
+			<tr key={ticket.id}>
+				<td>
+					<Link href='/tickets/[ticketId]' as={`/tickets/${ticket.id}`}>
+						<a>{ticket.title}</a>
+					</Link>
+				</td>
+				<td>{ticket.price}</td>
+			</tr>
+		);
+	});
 
-  return (
-    <div>
-      <h2>Tickets</h2>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Tickets</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>{ticketList}</tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Stubs</h2>
+			<table className='table'>
+				<thead>
+					<tr>
+						<th>Stub</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>{ticketList}</tbody>
+			</table>
+		</div>
+	);
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  const { data } = await client.get('/api/tickets');
+	const { data } = await client.get('/api/tickets');
 
-  return { tickets: data };
+	return { tickets: data };
 };
 
 export default LandingPage;
